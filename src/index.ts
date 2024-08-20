561e163af0a82116334dd9549a45eef61a502d68
XPoweR29 <xpower-29@wp.pl>
2024-08-20 16:59:20 +0200

    vercel.json cors configuration

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,10 @@
 import express from "express";
 import { mailRouter } from "./routes/mailRouter";
-import cors from "cors";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "https://webcraft-studio.pl");
-	res.header("Access-Control-Allow-Credentials", "true");
-	next();
-});
 
 app.use(express.json());
 
