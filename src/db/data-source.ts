3f9f9a33cacd7470924ad499dd00c35b596d346c
XPoweR29 <xpower-29@wp.pl>
2024-08-30 16:40:47 +0200

    Created singletone db instance and configured async server launch

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -4,16 +4,35 @@ import { DataSource } from "typeorm";
 dotenv.config();
 
 
-export const AppDataSource = new DataSource({
-	type: "mysql",
-	host: process.env.ADONA_DB_HOSTNAME,
-	port: 3306,
-	username: process.env.ADONA_DB_USER,
-	password: process.env.ADONA_DB_PASS,
-	database: process.env.ADONA_DB_DATABASE,
-	synchronize: false,
-	logging: true,
-	entities: ["src/entities/*.js"],
-	migrations: [],
-	subscribers: [],
-});
+let dataSource: DataSource | null = null;
+
+export const initializeDatabase = async (): Promise<DataSource> => {
+	if (dataSource && dataSource.isInitialized) {
+		console.log("Data Source is already initialized 💾 ➡ ✅");
+		return dataSource;
+	}
+
+	try {
+		dataSource = new DataSource({
+			type: "mysql",
+			host: process.env.ADONA_DB_HOSTNAME,
+			port: 3306,
+			username: process.env.ADONA_DB_USER,
+			password: process.env.ADONA_DB_PASS,
+			database: process.env.ADONA_DB_DATABASE,
+			synchronize: false,
+			logging: false,
+			entities: ["src/entities/*.js"],
+			migrations: [],
+			subscribers: [],
+		});
+
+		await dataSource.initialize();
+		console.log("Data Source is already initialized 💾 ➡ ✅");
+
+		return dataSource;
+	} catch (err) {
+		console.error("Error during Data Source initialization ❌");
+		throw new Error(err);
+	}
+};
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,32 @@
-import express from "express";
+import express, { Express } from "express";
 import "express-async-errors";
 import { mailRouter } from "./routes/mailRouter";
 import { adonaRouter } from "./routes/ADONA/adonaRouter";
 import * as dotenv from "dotenv";
 import { handleError } from "./middlewares/handleError";
-import { initializeDatabase } from "./utils/db-initialization";
 import { initialLog } from "./utils/serverLog";
+import { initializeDatabase } from "./db/data-source";
 dotenv.config();
 
 export const port = process.env.PORT || 3000;
 
-initializeDatabase();
-const app = express();
-app.use(express.json());
+async function startServer(): Promise<Express> {
+	 initializeDatabase(); 
 
-app.use("/api", mailRouter);
-app.use("/api/adona", adonaRouter);
+	const app: Express = express(); 
+	app.use(express.json());
 
-app.use(handleError);
+	app.use("/api", mailRouter);
+	app.use("/api/adona", adonaRouter);
+	app.use(handleError);
 
-app.listen(port, initialLog);
+	// app.listen(port, initialLog); 
+	return app;
+}
 
-export default app;
+const appInstance = startServer().catch((err) => {
+	console.error("Failed to start server:", err);
+	process.exit(1); 
+});
+
+export default appInstance; 
